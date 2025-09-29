612060df64d22398c9e2f92cb858d1be4c6a0311
yennth142 <yennth142@fpt.com>
2025-09-29 14:44:26 +0700

    Initialize microservice with api-gateway discovery-server Eureka

---
+++ order-service/order-service/src/main/java/com/example/microservice/order_service/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.example.microservice.order_service.controller;
+
+import com.example.microservice.order_service.model.Order;
+import com.example.microservice.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderRepository orderRepository;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
---
+++ user-service/user-service/src/main/java/com/example/microservice/user_service/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.microservice.user_service.controller;
+import com.example.microservice.user_service.model.User;
+import com.example.microservice.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
